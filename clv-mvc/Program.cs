ff6a9de4f4560d894ae5359de6b9452e2115cd67
prayaas on SP6 <prayaas@outlook.com>
2020-04-30 23:04:21 -0700

    kestrel

--- clv-mvc/Program.cs
+++ clv-mvc/Program.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.FileProviders;
+using System.Net;
+using System.Reflection;
+using System.IO;
 
 namespace clv_mvc
 {
@@ -13,7 +18,47 @@ namespace clv_mvc
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+                .ConfigureLogging(factory =>
+                {
+                    factory.AddConsole();
+                    factory.AddDebug();
+                    factory.AddFilter("Console", level => level >= LogLevel.Information);
+                    factory.AddFilter("Debug", level => level >= LogLevel.Information);
+                })
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Loopback, 5001, listenOptions =>
+                    {
+                        var serverCertificate = LoadCertificate();
+                        listenOptions.UseHttps(serverCertificate); // <- Configures SSL
+                    });
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+
+        }
+
+        private static X509Certificate2 LoadCertificate()
+        {
+            var assembly = typeof(Startup).GetTypeInfo().Assembly;
+            var embeddedFileProvider = new EmbeddedFileProvider(assembly, "clv_mvc");
+            var certificateFileInfo = embeddedFileProvider.GetFileInfo("cosmolab.pfx");
+            using (var certificateStream = certificateFileInfo.CreateReadStream())
+            {
+                byte[] certificatePayload;
+                using (var memoryStream = new MemoryStream())
+                {
+                    certificateStream.CopyTo(memoryStream);
+                    certificatePayload = memoryStream.ToArray();
+                }
+
+                return new X509Certificate2(certificatePayload, Environment.GetEnvironmentVariable("KEYPASS"));
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
