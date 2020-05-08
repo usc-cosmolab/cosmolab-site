f0e28358f1e97e092b8c7c0330287e0da89607c9
prayaas on SP6 <prayaas@outlook.com>
2020-05-08 04:14:19 -0700

    added omega_cdm, omega_k, removed omega_b

--- InputFileGenerator/Program.cs
+++ InputFileGenerator/Program.cs
@@ -14,23 +14,33 @@ namespace InputFileGenerator
             int count = 0;
             for(double h = 0.3; h <= 1.5; h += 0.2)
             {
-                for(double ob = 0.005; ob <= 0.039; ob += 0.004)
-                {
-                    string temp = text;
-                    FileStream fsn = new FileStream("h" + h.ToString("0.0") + "ob" + ob.ToString("0.000") + ".ini", FileMode.Create);
-                    if (temp.Contains("%p1%"))
-                        temp = temp.Replace("%p1%", h.ToString("0.0"));
-                    if (temp.Contains("%p2%"))
-                        temp = temp.Replace("%p2%", ob.ToString("0.000"));
+                //for(double ob = 0.005; ob <= 0.039; ob += 0.004)
+                //{
+                    for(double ocdm = 0; ocdm <= 1; ocdm += 0.2)
+                    {
+                        for(double ok = -0.5; ok <= 0.5; ok += 0.2)
+                        {
+                            string temp = text;
+                            FileStream fsn = new FileStream("h" + h.ToString("0.0") + /*"b" + ob.ToString("0.000") +*/ "cdm" + ocdm.ToString("0.0") + "k" + ok.ToString("0.0") + ".ini", FileMode.Create);
+                            if (temp.Contains("%p1%"))
+                                temp = temp.Replace("%p1%", h.ToString("0.0"));
+                            //if (temp.Contains("%p2%"))
+                            //    temp = temp.Replace("%p2%", ob.ToString("0.000"));
+                            if (temp.Contains("%p3%"))
+                                temp = temp.Replace("%p3%", ocdm.ToString("0.0"));
+                            if (temp.Contains("%p4%"))
+                                temp = temp.Replace("%p4%", ok.ToString("0.0"));
 
-                    using StreamWriter writer = new StreamWriter(fsn);
+                            using StreamWriter writer = new StreamWriter(fsn);
 
-                    writer.Write(temp);
-                    writer.Flush();
-                    fsn.Flush();
-                    writer.Close();
-                    count++;
-                }
+                            writer.Write(temp);
+                            writer.Flush();
+                            fsn.Flush();
+                            writer.Close();
+                            count++;
+                        }
+                    }
+                //}
             }
         }
 
--- Parser/Program.cs
+++ Parser/Program.cs
@@ -7,34 +7,45 @@ namespace Parser
     public class Program
     {
         static List<Output> outputValues;
+        const double MAGICERRORNUMBER = 69420.00;
 
-        public static List<Output> ProcessAndGetOutput(double h, double ob)
+        public static List<Output> ProcessAndGetOutput(double h, double ocdm, double ok)
         {
             string suffix = "00_cl.dat";
-            string filename = "h" + h.ToString("0.0") + "ob" + ob.ToString("0.000") + suffix;
+            string filename = "h" + h.ToString("0.0") + "cdm" + ocdm.ToString("0.0") + "k" + ok.ToString("0.0") + suffix;
             filename = "Vis_files/" + filename;
 
             outputValues = new List<Output>();
 
-            FileStream fs = new FileStream(filename, FileMode.Open);
-            using StreamReader reader = new StreamReader(fs);
-            int lines = 0;
-            while (!reader.EndOfStream)
+            try
             {
-                lines++;
-                Output lineC = new Output();
-                string line = reader.ReadLine();
-                if (line.StartsWith('#'))
-                    continue;
-                line = line.Substring(11);
-                for (int i = 0; i < 7; i++)
+                FileStream fs = new FileStream(filename, FileMode.Open);
+                using StreamReader reader = new StreamReader(fs);
+                int lines = 0;
+                while (!reader.EndOfStream)
                 {
-                    if (i != 0)
-                        line = line.Substring(25);
-                    string another = line.Substring(0, 19);
-                    lineC.AddNumber(i, Double.Parse(another, System.Globalization.NumberStyles.Float));
+                    lines++;
+                    Output lineC = new Output();
+                    string line = reader.ReadLine();
+                    if (line.StartsWith('#'))
+                        continue;
+                    line = line.Substring(11);
+                    for (int i = 0; i < 7; i++)
+                    {
+                        if (i != 0)
+                            line = line.Substring(25);
+                        string another = line.Substring(0, 19);
+                        lineC.AddNumber(i, Double.Parse(another, System.Globalization.NumberStyles.Float));
+                    }
+                    outputValues.Add(lineC);
                 }
-                outputValues.Add(lineC);
+            }
+            catch (FileNotFoundException)
+            {
+                outputValues = new List<Output>();
+                Output l = new Output();
+                l.AddNumber(0, MAGICERRORNUMBER);
+                outputValues.Add(l);
             }
 
             return outputValues;
